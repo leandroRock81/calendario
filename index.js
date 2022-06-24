a588ea5351cfad31b73363a971bf9541333e1c3c
Leandro Adriano <lerockadriano@gmail.com>
2022-06-24 11:07:22 -0300

    alteracao

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let min = data.getMinutes()
 
 if (hora >= 6 && hora <= 12){
     foto.src = 'imagens/manha.jpg'
-    msg.innerHTML = `Bom dia ${hora}:${min} ${dia}`
+    msg.innerHTML = `Bom dia ${hora}:${min}`
 
 }else if (hora > 12 && hora <= 18){
     foto.src = 'imagens/tarde.jpg'
